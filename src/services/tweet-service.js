138036da2efc7bb0f112c6a865089b7fc73ebf74
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-18 01:04:02 +0530

    Added tweet-service

---
+++ src/services/tweet-service.js
@@ -0,0 +1,20 @@
+const { TweetRepository } = require('../repository/index')
+
+
+class TweetService {
+    constructor () {
+        this.tweetRepository = new TweetRepository();
+    }
+    
+    async create(data) {
+        const content = data.content;
+        const tags = content.match(/#[a-zA-Z0-9_]+/g);         // A regex matching hashtags in content
+        tags = tags.map(tag => tag.substring(1));
+        console.log(tags);
+        
+        const tweet = await this.tweetRepository.create(data);
+        return tweet;
+    }
+}
+
+module.exports = TweetService;
