efabfc93087559fca0974c5200be2a8c187ed6f5
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-18 22:05:18 +0530

    Added tweet service with hashtag processing

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,16 +1,26 @@
-const { TweetRepository } = require('../repository/index')
+const { TweetRepository, HashtagRepository } = require('../repository/index')
 
 
 class TweetService {
     constructor () {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
     
     async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g);         // A regex matching hashtags in content
+        let tags = content.match(/#[a-zA-Z0-9_]+/g);         // A regex matching hashtags in content
         tags = tags.map(tag => tag.substring(1));
-        console.log(tags);
+        
+        const alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        let newTags = tags.filter(tag => !alreadyPresentTags.includes(tag));
+        newTags = newTags.map(tag => ({ title: tag , tweets: [tweet.id]}));
+        
+        alreadyPresentTags.forEach(tag => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        })
+        const newHashtags = await this.hashtagRepository.bulkCreate(newTags);
         
         const tweet = await this.tweetRepository.create(data);
         return tweet;
